9906ae29a2ee125c8a1509a21f3f522e87aef487
Sigrid Ho <hsinruiho@gmail.com>
2017-03-21 22:32:17 -0400

    parameters and sql

---
+++ cube_params.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# This file contains the various parameters used and default table names
+
+#Initialization database parameters
+CUBE_DB = "postgres"
+CUBE_DB_USER = "postgres"
+CUBE_DB_PASS = "password"
+CUBE_DB_PORT = 5432
---
+++ cube_sql.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import psycopg2
+import sys
+from cube_params import *
+
+def cube_db_initialize():
+    db_conn = psycopg2.connect("host='/tmp' dbname=%s user=%s password=%s port=%d" % (CUBE_DB, CUBE_DB_USER, CUBE_DB_PASS, CUBE_DB_PORT))
+    print "Connected To Database"
+    return db_conn
