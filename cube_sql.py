e010800d653bea9a961fff85e20cc4a9ed94cded
Haoming Chen <chenhaomingsz@gmail.com>
2017-03-27 11:42:19 -0400

    host on server

--- cube_sql.py
+++ cube_sql.py
@@ -5,6 +5,7 @@ from cube_params import *
 
 def cube_db_initialize():
     db_conn = psycopg2.connect("host='localhost' dbname=%s user=%s password=%s port=%d" % (CUBE_DB, CUBE_DB_USER, CUBE_DB_PASS, CUBE_DB_PORT))
+    # db_conn = psycopg2.connect("host='/tmp' dbname=%s user=%s password=%s port=%d" % (CUBE_DB, CUBE_DB_USER, CUBE_DB_PASS, CUBE_DB_PORT))
     print "Connected To Database"
     return db_conn
 
