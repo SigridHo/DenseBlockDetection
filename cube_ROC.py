e6e091f1f7090ca0c707abe34d5040d4a6999da7
Haoming Chen <chenhaomingsz@gmail.com>
2017-04-24 12:26:25 -0400

    fetch stats

--- cube_ROC.py
+++ cube_ROC.py
@@ -8,18 +8,25 @@ def readBlocks(block_index):
     num_benign = 0   # number of benign connection in the dense block
     num_attack = 0
     benignLabels = ["-", "normal."]
+    LABEL = {}
 
     table_name = BLOCK_TABLE + str(block_index)
     denseBlock = cube_sql_fetchRows(db_conn, table_name)
 
     for row in denseBlock:
         trueLabel = labels[row]
+        if trueLabel in LABEL:
+            LABEL[trueLabel] += 1
+        else:
+            LABEL[trueLabel] = 1
+
         if trueLabel in benignLabels:
             num_benign += 1
         else:
             num_attack += 1
 
     print "dense blocks #%d: benign=%d, attack=%d" % (block_index, num_benign, num_attack)
+    # print LABEL
     return num_benign, num_attack
 
 def readLabels(file):
@@ -84,6 +91,7 @@ def main():
     labels = {} 
     file = sys.argv[1] 
     fileName = 'DARPA TCP Dump'
+    # fileName = 'AirForce TCP Dump'
     maxNumBlocks = int(sys.argv[2])
     # read the true labels of each record from dataset
     total_num_benign, total_num_attack = readLabels(file)
@@ -105,43 +113,43 @@ def main():
 
         for i in range(maxNumBlocks):
             num_benign, num_attack = readBlocks(i)
-            num_records_inBlock = num_benign + num_attack
-            num_records_left -= num_records_inBlock
+        #     num_records_inBlock = num_benign + num_attack
+        #     num_records_left -= num_records_inBlock
 
-            accum_benign_inBlock += num_benign  # FP
-            accum_attack_inBlock += num_attack  # TP
+        #     accum_benign_inBlock += num_benign  # FP
+        #     accum_attack_inBlock += num_attack  # TP
 
-            falseNegative = total_num_attack - accum_attack_inBlock # FN
-            truePositive.append(accum_attack_inBlock)
-            trueNegative.append(num_records_left - falseNegative)   # TN
+        #     falseNegative = total_num_attack - accum_attack_inBlock # FN
+        #     truePositive.append(accum_attack_inBlock)
+        #     trueNegative.append(num_records_left - falseNegative)   # TN
 
-            fp, tp = computeStat(accum_benign_inBlock, accum_attack_inBlock, total_num_benign, total_num_attack)
-            falsePostive_rate.append(fp)
-            truePositive_rate.append(tp)
+        #     fp, tp = computeStat(accum_benign_inBlock, accum_attack_inBlock, total_num_benign, total_num_attack)
+        #     falsePostive_rate.append(fp)
+        #     truePositive_rate.append(tp)
 
-        print "falsePostive_rate: \n", falsePostive_rate
-        print "truePositive_rate: \n", truePositive_rate
-        plotROC(falsePostive_rate, truePositive_rate, fileName)
+        # print "falsePostive_rate: \n", falsePostive_rate
+        # print "truePositive_rate: \n", truePositive_rate
+        # plotROC(falsePostive_rate, truePositive_rate, fileName)
 
-        # compute accuracy using linear interpolation 
-        AUC = []
-        for i in range(maxNumBlocks - 1):
-            TP = truePositive[i] + truePositive[i + 1]
-            TN = trueNegative[i] + trueNegative[i + 1]
-            accuracy = (TP + TN) * 0.5 / (total_num_benign + total_num_attack)
-            AUC.append(accuracy)
-        X = [((2 * i + 1.0) / 2) for i in range(1, maxNumBlocks)]
-        plt.figure(2)
-        plt.title('Accuracy - %s' % fileName)  
-        plt.xlabel('Number of blocks (k)')  
-        plt.ylabel('Accuracy')  
-        plt.axis([1, 20, 0.0, 1.0])
-        plt.xticks([i for i in range(1, maxNumBlocks + 1)])
-        plt.yticks([i * 0.1 for i in range(0, 11)])
-        plt.plot(X, AUC, 'r', marker = 'x')  
-        plt.grid()  
-        # plt.show() 
-        plt.savefig('./Acuuracy_%s.pdf' % fileName)
+        # # compute accuracy using linear interpolation 
+        # AUC = []
+        # for i in range(maxNumBlocks - 1):
+        #     TP = truePositive[i] + truePositive[i + 1]
+        #     TN = trueNegative[i] + trueNegative[i + 1]
+        #     accuracy = (TP + TN) * 0.5 / (total_num_benign + total_num_attack)
+        #     AUC.append(accuracy)
+        # X = [((2 * i + 1.0) / 2) for i in range(1, maxNumBlocks)]
+        # plt.figure(2)
+        # plt.title('Accuracy - %s' % fileName)  
+        # plt.xlabel('Number of blocks (k)')  
+        # plt.ylabel('Accuracy')  
+        # plt.axis([1, 20, 0.0, 1.0])
+        # plt.xticks([i for i in range(1, maxNumBlocks + 1)])
+        # plt.yticks([i * 0.1 for i in range(0, 11)])
+        # plt.plot(X, AUC, 'r', marker = 'x')  
+        # plt.grid()  
+        # # plt.show() 
+        # plt.savefig('./Acuuracy_%s.pdf' % fileName)
 
     except:
         print "Unexpected error:", sys.exc_info()[0]    
@@ -163,4 +171,5 @@ if __name__ == '__main__':
     """
     main()
     # copyTables('DARPA')
+    # copyTables('AirForce')
 
